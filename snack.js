5654681905615579253100c91438a7923031397e
FadiAlashqar <fadialshqar.fa01@gmail.com>
2025-07-16 10:28:11 +0200

    [UPD]  snack 1.3

--- snack.js
+++ snack.js
@@ -6,8 +6,14 @@
 // const numSum = somma(2, 2);
 // console.log(numSum);
 
-const somma = function (num1, num2) {
-    return num1 + num2
-};
+// SNACK 1.2
+// const somma = function (num1, num2) {
+//     return num1 + num2
+// };
 
-console.log(somma(2, 5));
+// console.log(somma(2, 5));
+
+// SNACK 1.3
+
+const somma = (num1, num2) => num1 + num2;
+console.log(somma(3, 2));
