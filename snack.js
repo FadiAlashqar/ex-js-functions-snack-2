d2e3cc08e32ce47985ce93e294dc21f098b400b0
FadiAlashqar <fadialshqar.fa01@gmail.com>
2025-07-16 10:20:01 +0200

    [UPD] snack 1.2

--- snack.js
+++ snack.js
@@ -1,7 +1,13 @@
 // SNACK 1
-function somma(num1, num2) {
+// function somma(num1, num2) {
+//     return num1 + num2
+// };
+
+// const numSum = somma(2, 2);
+// console.log(numSum);
+
+const somma = function (num1, num2) {
     return num1 + num2
 };
 
-const numSum = somma(2, 2);
-console.log(numSum);
+console.log(somma(2, 5));
